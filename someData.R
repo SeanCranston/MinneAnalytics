73eb3bb4e6c785c7a42e6e3fd76c6052aae44c0f
SeanCranston <seancranston0@gmail.com>
2021-03-11 10:29:08 -0600

    cleaned stat2019

--- someData.R
+++ someData.R
@@ -14,6 +14,18 @@ stats2019 <- reduced_tbl
 View(stats2019)
 
 
+stats2019 <- stats2019[-c(seq(22,374,by = 22),seq(23,374,by = 22)),] #removed unnecessary rows
+
+names(stats2019) <- paste(names(stats2019),stats2019[1,]) #concatenate the colnames with the first row 
+
+stats2019 <- stats2019[-c(1),] #remove 1st row that contains all colnames
+
+#remove NA columns
+stats2019 <- stats2019[,!duplicated(colnames(stats2019))] #thought it would be easier to remove the bulk and then remove the remaining one
+stats2019 <- stats2019[,-c(9)] 
+
+view(stats2019)
+
 # Years with NCAA March Madness Tournament and Have Stats not separated by conference
 # There are many years on the website that have season stats but they are separated by conference into many tables
 #years <- c(1993:2019)
