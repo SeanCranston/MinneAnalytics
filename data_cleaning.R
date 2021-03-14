74aa2af0db807a6a931cda7653373822dbae1f0b
Sean Cranston <seancranston0@gmail.com>
2021-03-14 08:43:11 -0500

    model DF

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,35 +1,35 @@
 library(tidyverse)
 library(readxl)
 
-#if someone can think of better var name dont be afraid to use it
-
-#Cleaning season 2020-2021
-Year0 <- read_excel("NCAA Statistics.xlsx",sheet = "2020-2021")
-#the w-L column didn't come in right
-View(Year0)
-
-
-#cleaning season 2019-2020
-Year1 <- read_excel("NCAA Statistics.xlsx", sheet = "2019-2020")
-#split the W-L column into two columns 
-a <- str_split(Year1$`W-L`,"-")
-for (i in 1:dim(Year1)[1]) {
-    Year1$W[i] <- a[[i]][1] %>% as.numeric()
-    Year1$L[i] <- a[[i]][2] %>% as.numeric()
-}
-Year1 <- Year1 %>% mutate('W-L'=NULL) #removed W-L column
-
-
-
-#Cleaning season 2018-2019
-Year2 <- read_excel("NCAA Statistics.xlsx", sheet = "2018-2019")
-#split the W-L column into two columns 
-b <- str_split(Year2$`W-L`,"-")
-for (i in 1:dim(Year2)[1]) {
-    Year2$W[i] <- b[[i]][1] %>% as.numeric()
-    Year2$L[i] <- b[[i]][2] %>% as.numeric()
-}
-Year2 <- Year2 %>% mutate('W-L'=NULL) #removed W-L column
+# #if someone can think of better var name dont be afraid to use it
+# 
+# #Cleaning season 2020-2021
+# Year0 <- read_excel("NCAA Statistics.xlsx",sheet = "2020-2021")
+# #the w-L column didn't come in right
+# View(Year0)
+# 
+# 
+# #cleaning season 2019-2020
+# Year1 <- read_excel("NCAA Statistics.xlsx", sheet = "2019-2020")
+# #split the W-L column into two columns 
+# a <- str_split(Year1$`W-L`,"-")
+# for (i in 1:dim(Year1)[1]) {
+#     Year1$W[i] <- a[[i]][1] %>% as.numeric()
+#     Year1$L[i] <- a[[i]][2] %>% as.numeric()
+# }
+# Year1 <- Year1 %>% mutate('W-L'=NULL) #removed W-L column
+# 
+# 
+# 
+# #Cleaning season 2018-2019
+# Year2 <- read_excel("NCAA Statistics.xlsx", sheet = "2018-2019")
+# #split the W-L column into two columns 
+# b <- str_split(Year2$`W-L`,"-")
+# for (i in 1:dim(Year2)[1]) {
+#     Year2$W[i] <- b[[i]][1] %>% as.numeric()
+#     Year2$L[i] <- b[[i]][2] %>% as.numeric()
+# }
+# Year2 <- Year2 %>% mutate('W-L'=NULL) #removed W-L column
 
 
 
@@ -56,12 +56,13 @@ for (i in 1:2) { #only going through 1:2 to debug
 }
 
 # this jsut gets rid of that nasty html stuff (I think that's what it is anyway)
+df <- gsub("\n.","",df)
 df <- gsub("\n","",df)
 df <- gsub("  ","",df) 
 df <- gsub("FINAL","",df)
 df <- as_tibble(df)
 
-# making the data more user friendly
+# spliting the string into 4 columns (by team and score)
 scores <- c()
 teams <- c()
 for (i in 1:dim(df)[1]) {
@@ -82,34 +83,54 @@ colnames(games_team) <- c("team 1","team 2")
 games <- cbind(games_team,games_score) %>% as_tibble()
 
 
+--------------------------------------------------------------------------------
+# I don't think theirs an easy way to fix the code above
+    #https://www.sports-reference.com/cbb/boxscores/index.cgi?month=03&day=1&year=2021
+url1 <- "https://www.sports-reference.com/cbb/boxscores/index.cgi?month="
+day <- "&day="
+year <- "&year="
+
+url <- c()
+df <- c()
+for (i in 1:2) { #day
+    
+    url[i] <- paste0(url1,"3",day,i,year,"2021")###############################
+    lis[[i]] <- url[i] %>%
+        read_html() %>%
+        html_nodes(".gamePod.gamePod-type-game.status-final") %>%
+        html_text()
+    
+    df <- c(df,lis[[i]]) 
+    #we don't want the data in a list, and it would get to messy me to concentatnate the lis variable in the loop (I think anyways)
+}
 
 
 
----------------------------------------------------------------------------------
-#this is the same data Nicole found
-# from http://web1.ncaa.org/stats/StatsSrv/rankings
-
-WL_per <- read_csv("rankings.csv",skip = 11,n_max = 354)
-WL_per <- WL_per[-c(351),]
-WL_per$Rank <- c(seq(1,350),rep("NR",3))
-View(WL_per)
-
-Iscor_Offense <- read_csv("rankings.csv",skip = 372,n_max = 354,
-                          col_types = cols(
-                              Rank = col_character(), #bc we have some unranked teams
-                              Name = col_character(),
-                              GM = col_double(),
-                              `W-L` = col_character(),
-                              PTS = col_double(),
-                              PPG = col_double()
-                          ))
-Iscor_Offense <- Iscor_Offense[-c(351),]
-Iscor_Offense$Rank <- c(seq(1,350),rep("NR",3))
-view(Iscor_Offense)
-# PTS ~ cumulative points in season
-# PPG ~ average points per game
-
-Iscor_Defense <- 
+# ---------------------------------------------------------------------------------
+# #this is the same data Nicole found
+# # from http://web1.ncaa.org/stats/StatsSrv/rankings
+# 
+# WL_per <- read_csv("rankings.csv",skip = 11,n_max = 354)
+# WL_per <- WL_per[-c(351),]
+# WL_per$Rank <- c(seq(1,350),rep("NR",3))
+# View(WL_per)
+# 
+# Iscor_Offense <- read_csv("rankings.csv",skip = 372,n_max = 354,
+#                           col_types = cols(
+#                               Rank = col_character(), #bc we have some unranked teams
+#                               Name = col_character(),
+#                               GM = col_double(),
+#                               `W-L` = col_character(),
+#                               PTS = col_double(),
+#                               PPG = col_double()
+#                           ))
+# Iscor_Offense <- Iscor_Offense[-c(351),]
+# Iscor_Offense$Rank <- c(seq(1,350),rep("NR",3))
+# view(Iscor_Offense)
+# # PTS ~ cumulative points in season
+# # PPG ~ average points per game
+# 
+# Iscor_Defense <- 
 
 
 
---
+++ model_data.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+
+source("data_cleaning.R",echo = FALSE)
